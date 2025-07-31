d7b9693a8ca6d372da07ec16ebd1e248d04a2a5f
Joao Gabriel Gomes <joao.gomes@lgepartner.com>
2025-07-31 10:47:11 -0400

    .

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -4,13 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from '../routes/app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { bookServiceProvider } from '../books/service/book.provider.service';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    bookServiceProvider, provideHttpClient()
+    bookServiceProvider, provideHttpClient(withFetch())
   ]
 };
